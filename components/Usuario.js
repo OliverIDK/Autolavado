0413e8f7111f1056d18dcb317e6b71261bba44b6
VaneeeIDK <vanessahdeztopete@gmail.com>
2024-11-10 10:28:11 -0600

    Se realizo la vista de los usuarios

--- components/Usuario.js
+++ components/Usuario.js
@@ -1,18 +1,70 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { database } from '../src/config/fb'
 import { deleteDoc, doc } from 'firebase/firestore'
 import { AntDesign } from '@expo/vector-icons'
-const Usuario = ({id, name, rol,
+import Icon from '@expo/vector-icons/Entypo';
+const Usuario = ({ id, name, rol, email,
+
 }) => {
   return (
-    <View>
-      <Text>{name}</Text>
-      <Text>{rol}</Text>
-    </View>
+      <View style={styles.celdas}>
+
+        <View style={styles.leftContainer}>
+          <Icon name="user" size={25} color="#888" style={styles.icon} />
+          <View style={styles.textContain}>
+            <Text style={styles.txtName}>{name}</Text>
+            <Text style={styles.txtRol}>{rol}</Text>
+          </View>
+        </View>
+
+        <View>
+          <TouchableOpacity>
+            <Icon name="dots-three-horizontal" size={25} color="#888" style={styles.icon} />
+          </TouchableOpacity>
+        </View>
+
+      </View>
   )
 }
 
 export default Usuario
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+  celdas: {
+    padding: 10,
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    height: 'auto',
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    gap: 10
+  },
+  leftContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 10
+  },
+  textContain: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  icon: {
+    padding: 10,
+  },
+  txtName: {
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  txtRol:{
+    fontSize: 12,
+  }
+})
--- screens/Usuarios.js
+++ screens/Usuarios.js
@@ -1,8 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { database } from '../src/config/fb';
 import { collection, onSnapshot, query } from 'firebase/firestore';
 import Usuario from '../components/Usuario';
+import Icon from '@expo/vector-icons/Entypo';
 
 const Usuarios = () => {
   const [users, setUsers] = useState([]);
@@ -16,7 +17,7 @@ const Usuarios = () => {
           id: doc.id,
           name: doc.data().name,
           email: doc.data().email,
-          rol: doc.data().rol 
+          rol: doc.data().rol
         }))
       );
     });
@@ -26,7 +27,18 @@ const Usuarios = () => {
 
   return (
     <View style={styles.container}>
-      {users.map(user => <Usuario key={user.id} {...user} />)}
+      <View style={styles.header}>
+        <View style={styles.inputContainer}>
+          <Icon name="magnifying-glass" size={20} color="#888" style={styles.icon} />
+          <TextInput style={styles.input} placeholder="Buscar usuario" />
+        </View>
+      </View>
+      <View style={styles.body}>
+        {users.map(user => <Usuario key={user.id} {...user} />)}
+      </View>
+      <TouchableOpacity style={styles.btnAdd}>
+        <Icon name="circle-with-plus" size={70} color="#144E78" style={styles.icon} />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -35,11 +47,54 @@ export default Usuarios;
 
 const styles = StyleSheet.create({
   container: {
-    padding: 16,
+    width: '100%',
+    height: '100%',
+    position: 'relative',
   },
   header: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 8,
+    width: '100%',
+    height: 100,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputContainer: {
+    flexDirection: "row", // Los elementos dentro de este contenedor estarán en fila
+    alignItems: "center", // Centra verticalmente el ícono y el input
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    margin: 15,
+    width: 350,
+    height: 50,
+  },
+  input: {
+    flex: 1,
+    fontSize: 15,
+  },
+  icon: {
+    padding: 10,
+  },
+  body: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'white',
+    borderTopWidth: 1,
+    borderColor: '#ccc',
+  },
+  btnAdd: {
+    
+    alignItems: "center",
+    justifyContent: 'center',
+    borderRadius: 100,
+    position: 'absolute',
+    right: 10,
+    bottom: 10,
+
+  },
+  btnText: {
+    color: "white",
+    fontSize: 40,
+    fontWeight: "bold",
   },
 });
