ff2fd4bfb8402f8a28afa6d87666adf42b23ff46
OliverIDK <oliveratoon@gmail.com>
2024-12-07 19:06:53 -0600

    esperemos que esto no pete

--- app.config.js
+++ app.config.js
@@ -17,6 +17,7 @@ export default {
       "supportsTablet": true
     },
     "android": {
+      "package": "com.anonymous.autolavado",
       "adaptiveIcon": {
         "foregroundImage": "./assets/adaptive-icon.png",
         "backgroundColor": "#ffffff"
--- src/config/fb.js
+++ src/config/fb.js
@@ -1,8 +1,9 @@
-import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import Constants from "expo-constants";
- 
+
 const firebaseConfig = {
   apiKey: Constants.expoConfig.extra.apiKey,
   authDomain: Constants.expoConfig.extra.authDomain,
@@ -12,8 +13,11 @@ const firebaseConfig = {
   appId: Constants.expoConfig.extra.appId,
   databaseURL: Constants.expoConfig.extra.databaseURL,
 };
- 
+
 const app = initializeApp(firebaseConfig);
 
 export const database = getFirestore(app);
-export const auth = getAuth(app);
+
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
